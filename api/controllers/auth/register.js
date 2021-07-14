ffa55fe541a3fc55ce6c3882e665f7e19d860828
balazs.tomkovics <tomkovicsb@gmail.com>
2021-07-14 15:28:47 +0200

    Added api docs and pm2 files

--- api/controllers/auth/register.js
+++ api/controllers/auth/register.js
@@ -3,6 +3,22 @@ const Response = require('../../../services/response');
 
 const { MissingParamsException } = require('../../../services/error');
 
+/**
+ * @api {post} /auth/register Email registration
+ * @apiDescription Email and password based registration
+ * @apiName Email registration
+ * @apiGroup Authentication
+ * @apiPermission public
+ *
+ * @apiParam {String} email Email.
+ * @apiParam {String} password Password. At least 8 characters long and must contain letters and numbers.
+ *
+ * @apiSuccess {Boolean} isSuccess The requested operation was successful or not
+ * @apiSuccess {Object} error If isSuccess equals false
+ * @apiSuccess {String} error.message Error message
+ * @apiSuccess {Number} error.code Error code
+ */
+
 module.exports = async (req, res) => {
   const { body } = req;
   const response = new Response(res);
--- api/controllers/user/updateUser.js
+++ api/controllers/user/updateUser.js
@@ -5,6 +5,30 @@ const mongo = require('../../../services/mongo');
 const User = mongo.get('User');
 const cacheConfig = config.cache;
 
+/**
+ * @api {post} /user Update user
+ * @apiDescription Update the authenticated user's profile data
+ * @apiName Update user
+ * @apiGroup User
+ * @apiPermission jwt
+ *
+ * @apiParam {String} email User's email.
+ * @apiParam {String} nickname User's nickname.
+ * @apiParam {String} firstName User's first name.
+ * @apiParam {String} lastName User's last name.
+ *
+ * @apiSuccess {Boolean} isSuccess The requested operation was successful or not
+ * @apiSuccess {Object} result If isSuccess equals true
+ * @apiSuccess {String} result.userId User's db id.
+ * @apiSuccess {String} result.email User's email
+ * @apiSuccess {String} result.nickname User's nickname.
+ * @apiSuccess {String} result.firstName User's first name.
+ * @apiSuccess {String} result.lastName User's last name.
+ * @apiSuccess {Object} error If isSuccess equals false.
+ * @apiSuccess {String} error.message Error message.
+ * @apiSuccess {Number} error.code Error code.
+ */
+
 module.exports = async (req, res) => {
     const { user, body } = req;
     const response = new Response(res);
